ae3bb9249ae1078859def128a3d4737aebd25257
Max <MaxwellBels@gmail.com>
2016-05-08 15:55:33 +0300

    boot practice

---
+++ javascript/dz1-2/script/script.js
@@ -0,0 +1,12 @@
+alert("hi");
+
+function pow(x, n) {
+  var result = 1;
+  for (var i = 0; i < n; i++) {
+    result *x = x;
+  }
+  return result;
+}
+
+var x = prompt("x?", "");
+var n = prompt("n?", "");
