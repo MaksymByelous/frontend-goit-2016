c46434ed319b2f8e49eb23ee04718886f9f8c0a3
Max <MaxwellBels@gmail.com>
2016-05-10 22:54:20 +0300

    new

--- javascript/dz1-2/script/script.js
+++ javascript/dz1-2/script/script.js
@@ -1,12 +1,37 @@
-alert("hi");
-
+// ------block 1 of homework: lets get a power!
 function pow(x, n) {
   var result = 1;
   for (var i = 0; i < n; i++) {
-    result *x = x;
+    result *= x;
   }
+  console.log(result);
   return result;
 }
+var x = prompt("input x - number is raised to power", "");
+var n = prompt("input n - power of number", "");
+alert( pow(x, n) );
 
-var x = prompt("x?", "");
-var n = prompt("n?", "");
+// ------block 2 of homework: are you right user?
+var names= ["1", "2", "3", "4", "5"];
+function logIN () {
+  for (var i = 0; i < names.length; i++) {
+    names [i]= prompt("input next name", "");
+  } return names;
+}
+logIN();
+
+var userName= prompt("input user name ", "");
+function checkNames () {
+  for (var i = 0; i < names.length; i++) {
+    if(names[i]==userName) {
+    return true;
+    }
+  }
+}
+var rightUser = names.some(checkNames);
+if (rightUser) {
+  alert( userName + " you are welcome!")
+} else {
+  alert("you are not in the users list")
+  }
+  
