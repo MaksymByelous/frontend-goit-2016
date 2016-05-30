5c396d4a9539d7e838c47478f64f917361e52601
Max <MaxwellBels@gmail.com>
2016-05-30 18:23:23 +0300

    new

--- javascript/dz5-6/script/script.js
+++ javascript/dz5-6/script/script.js
@@ -1,73 +1,103 @@
+var btnStatus = {
+    start: 'Start',
+    pause: 'Pause',
+    continueT: 'Continue',
+    clear: 'Clear'
+};
+var timer = {
+    hour: 0,
+    minute: 0,
+    second: 0,
+    milisecond: 0
+};
+var watch = document.querySelector('.watch-timer');
+var timerHour = document.createElement('span');
+timerHour.innerHTML = '0' + timer.hour;
+watch.appendChild(timerHour);
+var dots = document.createElement('span');
+dots.innerHTML = ':';
+watch.appendChild(dots);
+var timerMinute = document.createElement('span');
+timerMinute.innerHTML = '0' + timer.minute;
+watch.appendChild(timerMinute);
+var dots2 = document.createElement('span');
+dots2.innerHTML = ':';
+watch.appendChild(dots2);
+var timerSecond = document.createElement('span');
+timerSecond.innerHTML = '0' + timer.second;
+watch.appendChild(timerSecond);
+var timerMilisecond = document.createElement('p');
+timerMilisecond.innerHTML = timer.milisecond;
+watch.appendChild(timerMilisecond);
+var startStopBtn = document.querySelector('.start-Pause-Cont');
+startStopBtn.value,
+startStopBtn.innerHTML = btnStatus.start;
+var clearBtn = document.querySelector('.clear');
+clearBtn.value,
+clearBtn.innerHTML = btnStatus.clear;
 
-
-
-
-
-
-
-
-
-// var test = {
-//     name: 'Test of development skills',
-//     question: ['question №1', 'question №2', ' question №3'],
-//     answer: ['answer option №1', 'answer option №2', 'answer option №3',
-//     'answer option №1', 'answer option №2', 'answer option №3',
-//     'answer option №1', 'answer option №2', 'answer option №3'],
-//     submit: 'CHECK RESULTS',
-//     letsTest: function() {
-//         var row = document.createElement('div');
-//         row.classList.add('row');
-//         var body = document.querySelector('body');
-//         body.appendChild(row);
-//         var col = document.createElement('div');
-//         col.classList.add('col');
-//         col.classList.add('col-md-12');
-//         var row = document.querySelector('.row');
-//         row.appendChild(col);
-//         var topH1 = document.createElement('h1');
-//         topH1.classList.add('text-center');
-//         topH1.innerHTML = this.name;
-//         var col = document.querySelector('.col');
-//         col.appendChild(topH1);
-//         var testForm = document.createElement('form');
-//         var col = document.querySelector('.col');
-//         col.appendChild(testForm);
-//         var questionIterator = 0;
-//         for (var y = 0; y < 3; y++) {
-//             var formGroup = document.createElement('div');
-//             formGroup.classList.add('form-group');
-//             var form = document.querySelector('form');
-//             form.appendChild(formGroup);
-//             var formH2 = document.createElement('h2');
-//             formH2.innerHTML = this.question[y];
-//             var formGroup = document.querySelectorAll('.form-group');
-//             formGroup[y].appendChild(formH2);
-//             var divCheckBox = document.createElement('div');
-//             divCheckBox.classList.add('checkbox');
-//             var formGroup = document.querySelectorAll('.form-group');
-//             formGroup[y].appendChild(divCheckBox);
-//             for (var i = 1; i < 4; i++) {
-//                 var checkbox = document.createElement('input');
-//                 checkbox.type = 'checkbox';
-//                 checkbox.value = 'answer' + i;
-//                 checkbox.id = 'answer' + (+y + 1) + i;
-//                 var divCheckBox = document.querySelectorAll('.checkbox');
-//                 divCheckBox[y].appendChild(checkbox);
-//                 var label = document.createElement('label');
-//                 label.innerHTML = this.answer[i - 1 + questionIterator];
-//                 label.htmlFor = 'answer' + (+y + 1) + i;
-//                 var divCheckBox = document.querySelectorAll('.checkbox');
-//                 divCheckBox[y].appendChild(label);
-//             }
-//             questionIterator += 3;
-//         }
-//         var button = document.createElement('button');
-//         button.classList.add('btn');
-//         button.classList.add('btn-success');
-//         button.classList.add('btn-lg');
-//         button.innerHTML = this.submit;
-//         var form = document.querySelector('form');
-//         form.appendChild(button);
-//     }
-// }
-// test.letsTest();
+var interval = 0;
+var iterator = 1;
+function timeOn() {
+    interval = setInterval(function() {
+        timerMilisecond.innerHTML = timer.milisecond.toString();
+        (timer.second.toString().length == 1) ? (timerSecond.innerHTML = '0' + timer.second) : (timerSecond.innerHTML = timer.second.toString());
+        (timer.minute.toString().length == 1) ? (timerMinute.innerHTML = '0' + timer.minute) : (timerMinute.innerHTML = timer.minute.toString());
+        (timer.hour.toString().length == 1) ? (timerHour.innerHTML = '0' + timer.hour) : (timerHour.innerHTML = timer.hour.toString());
+        if (iterator == 250) {
+            clearInterval(interval);
+            timer.second += 1;
+            iterator = 1;
+            timeOn();
+            if (timer.second == 60) {
+                timer.minute += 1;
+                timer.second = 0;
+                if (timer.minute == 60) {
+                    timer.hour += 1;
+                    timer.minute = 0;
+                }
+            }
+        }
+        iterator++;
+        timer.milisecond = iterator * 4 - 1;
+    }, 4);
+}
+function clearHandler() {
+    timer.hour = 0;
+    timer.minute = 0;
+    timer.second = 0;
+    timer.milisecond = 0;
+    timerMilisecond.innerHTML = timer.milisecond;
+    timerSecond.innerHTML = '0' + timer.second;
+    timerMinute.innerHTML = '0' + timer.minute;
+    timerHour.innerHTML = '0' + timer.hour;
+    clearInterval(interval);
+    var iterator = 1;
+    startStopBtn.value = btnStatus.start;
+    startStopBtn.innerHTML = btnStatus.start;
+    startStopBtn.classList.remove('btn-warning');
+    startStopBtn.classList.add('btn-success');
+}
+function handler() {
+    switch (startStopBtn.value) {
+    case btnStatus.start:
+        startStopBtn.value = btnStatus.pause;
+        startStopBtn.innerHTML = btnStatus.pause;
+        startStopBtn.classList.remove('btn-success');
+        startStopBtn.classList.add('btn-warning');
+        timeOn();
+        break;
+    case btnStatus.pause:
+        startStopBtn.value = btnStatus.continueT;
+        startStopBtn.innerHTML = btnStatus.continueT;
+        clearInterval(interval);
+        break;
+    case btnStatus.continueT:
+        startStopBtn.value = btnStatus.pause;
+        startStopBtn.innerHTML = btnStatus.pause;
+        timeOn();
+        break;
+    }
+}
+startStopBtn.addEventListener("click", handler);
+clearBtn.addEventListener("click", clearHandler);
