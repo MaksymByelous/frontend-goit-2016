f15ce824ba7eb0648785c887f01cb242bb0c7c9d
Max <MaxwellBels@gmail.com>
2016-08-16 12:07:24 +0300

    new

--- javascript/dz19-20_sass/js/script.js
+++ javascript/dz19-20_sass/js/script.js
@@ -1,12 +1,23 @@
 
 $( function() {
+
+//-----acrivate akordeon plugin
   $( "#accordion" ).accordion();
+//----open full text of article
+  $( "a.articles-read-more" ).click(function (e) {
+    e.preventDefault();
+    $(this).prev('p').toggleClass('height100');
+  });
 
-
-    $( "a.articles-read-more" ).click(function (e) {
-      e.preventDefault();
-      $(this).prev('p').toggleClass('height100');
-    });
+//-------slide down main menu
+$( '.dropdown' ).hover(
+        function(){
+            $(this).children('.sub-menu').slideDown(200);
+        },
+        function(){
+            $(this).children('.sub-menu').slideUp(200);
+        }
+    );
 
 });
 
