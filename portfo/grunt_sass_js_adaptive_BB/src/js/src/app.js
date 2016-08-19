b0f6a4dc460f44e26157ff369ab6b798e330f564
Max <MaxwellBels@gmail.com>
2016-08-19 18:45:35 +0300

    new

--- portfo/grunt_sass_js_adaptive_BB/src/js/src/app.js
+++ portfo/grunt_sass_js_adaptive_BB/src/js/src/app.js
@@ -1,7 +1,10 @@
 'use strict';
 $( function() {
-  alert('Hello! This page done with SASS on HTML5/CSS3. It is adaptive for desctop, tablet and mobile. It contains JQuery Plugin (slider), JQuery-UI widget (accordion), drop-down menu with JS, hover and click animation with easing (there are too many yellow trucks...try to click on it!). Project build with Grunt'
-  );
+  $('body').click(function () {
+    alert('Hello! This page done with SASS on HTML5/CSS3. It is adaptive for desctop, tablet and mobile. It contains JQuery Plugin (slider), JQuery-UI widget (accordion), drop-down menu with JS, hover and click animation with easing (there are too many yellow trucks...try to click on it!). Project build with Grunt'
+    );
+    $(this).unbind('click');
+  });
 //-----acrivate akordeon plugin
   $( "#accordion" ).accordion();
 //----open full text of article
