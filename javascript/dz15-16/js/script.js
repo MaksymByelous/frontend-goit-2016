d6c8fd3ad300a1b7258d64e1d95fb39531c2cf20
Max <MaxwellBels@gmail.com>
2016-09-01 12:46:57 +0300

    new

--- javascript/dz15-16/js/script.js
+++ javascript/dz15-16/js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
   var string = '';
   function googleSearch(e){
           $.ajax({
-          url:'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&hl=ru&rsz=large&q='+e+'&callback=GoogleCallback&context=?',
+          url:'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q='+e+'&callback=GoogleCallback&context=?',
           dataType: 'jsonp'
   });
 };
