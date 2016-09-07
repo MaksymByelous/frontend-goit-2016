51d7ed0656d4e26a6631c6ce9a6eefbee443ed5e
Max <MaxwellBels@gmail.com>
2016-09-07 15:44:38 +0300

    first

---
+++ javascript/Exam/src/js/src/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+
+//--------Masonsy plugin
+  $('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  });
+
+
+});
--- javascript/dz19-20_sass/gruntfile.js
+++ javascript/dz19-20_sass/gruntfile.js
@@ -58,50 +58,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  // grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.registerTask('default', ['concat_css', 'sass',  'concat', 'uglify', 'watch']);
 };
-
-// module.exports = function(grunt) {
-//
-//   grunt.initConfig({
-//     concat: {
-//       options: {
-//         separator: ';'
-//       },
-//       dist: {
-//         src: ['js/src/*.js'],
-//         dest: 'js/script.main.min.js'
-//        }
-//       },
-//     uglify: {
-//       dist: {
-//           src: ['js/script.main.min.js'],
-//           dest: 'js/script.main.min.js'
-//       }
-//     },
-//     concat_css: {
-//       options: {
-//         separator: ';'
-//       },
-//       dist: {
-//         src: ['css/src/*.css'],
-//         dest: 'css/styles.main.min.css'
-//        }
-//       },
-//     cssmin: {
-//       dist: {
-//           src: ['css/styles.main.min.css'],
-//           dest: 'css/styles.main.min.css'
-//       }
-//     }
-//   });
-//
-//   grunt.loadNpmTasks('grunt-contrib-concat');
-//   grunt.loadNpmTasks('grunt-contrib-uglify');
-//   grunt.loadNpmTasks('grunt-concat-css');
-//   grunt.loadNpmTasks('grunt-contrib-cssmin');
-//
-//   grunt.registerTask('default', ['concat', 'uglify', 'concat_css', 'cssmin']);
-//
-// };
