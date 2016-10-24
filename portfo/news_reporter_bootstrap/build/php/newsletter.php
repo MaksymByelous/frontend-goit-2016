9dde141c10364662725a6d97ce7e9eb0845ee16c
Max <MaxwellBels@gmail.com>
2016-10-24 11:20:26 +0300

    NR and Sprint added

---
+++ portfo/news_reporter_bootstrap/build/php/newsletter.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_POST['submit'])){
+    $to = "maxwellbels@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = "New submission for Newsletter";
+    $headers = "New submission for Newsletter from:" . $from;
+    mail($to,$subject,$headers);
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    }
+?>
---
+++ portfo/news_reporter_bootstrap/src/js/src/app.js
@@ -0,0 +1,21 @@
+'use strict';
+$( function() {
+
+//----jssocials plugin activate
+  $("#share").jsSocials({
+    showCount: true,
+    showLabel: true,
+    shares: ["twitter", "facebook", "googleplus", "linkedin"]
+  });
+//------search plugin
+  var search = $('input.search').searchMeme({ onSearch: function (searchText) {
+                setTimeout(function () {
+                    search.searchMeme({ searchComplete: true });
+                    alert('Search completed.');
+                }, 3000);
+            }
+            , buttonPlacement: 'left', button: 'red'
+  });
+
+
+});
