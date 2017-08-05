acb514dc814d0e393c5b02abd06fed1c6469ee2c
Max Bels <maxwellbels@gmail.com>
2017-08-05 16:01:10 +0200

    change logic getting data

--- parts/cinema/src/app/modules/landing/home/home.ctrl.js
+++ parts/cinema/src/app/modules/landing/home/home.ctrl.js
@@ -12,61 +12,59 @@
     vm.getMovies = getMovies;
     vm.movies = [];
     vm.activatePage = activatePage;
-    vm.pageIndex = 0;
+    vm.pageIndex = 1;
     vm.showAllPagination = false;
     vm.movieDetailedIndex;
-    vm.findMovieIndex = findMovieIndex;
+    vm.openMovieDetails = openMovieDetails;
     vm.movieDetails = vm.movies[vm.movieDetailedIndex];
     vm.details = false;
     vm.nextMovieDetails = nextMovieDetails;
 
+
     function getPages() {
       $http.jsonp("http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c").then(function(response) {
         vm.makePagination(response.data.total_pages);
-        vm.getMovies();
+        vm.getMovies(1);
       });
     }
     vm.getPages();
 
     function makePagination(max) {
-      vm.pagesBy12 = Math.round( ((max - 1) * 20)/ 12 );
-      for (var i = 0; i < vm.pagesBy12; i++) {
+      for (var i = 0; i < max; i++) {
         vm.pages[i] = i + 1;
       }
     }
 
-    function getMovies() {
-      for (var i = 0; i < vm.pages.length; i++) {
-        $http.jsonp("http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&page=" + (i + 1) ).then(function(response) {
-          for (var i = 0; i < response.data.results.length; i++) {
-            vm.movies.push(response.data.results[i]);
-          }
-          vm.activePage = angular.copy(vm.movies).splice(0, 12);
+    function getMovies(index) {
+        $http.jsonp("http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&page=" + index ).then(function(response) {
+          vm.activePage = angular.copy(response.data.results);
         });
-      }
+        vm.pageIndex = index;
     }
 
     function activatePage(index) {
-      if(index < 0 || index > (vm.pages.length - 1)) {
+      if(index < 1 || index > (vm.pages.length)) {
         return;
       }
-      vm.start = index * 12;
-      vm.activePage = angular.copy(vm.movies).splice(vm.start, 12);
       vm.pageIndex = index;
+      vm.getMovies(index);
     }
 
-    function findMovieIndex(movieid) {
-      if(vm.pageIndex === 0) {
-        vm.movieDetailedIndex = movieid;
-      } else {
-        vm.movieDetailedIndex = vm.pageIndex * 12 + movieid;
-      }
-      vm.movieDetails = vm.movies[vm.movieDetailedIndex];
+    function openMovieDetails(movieid) {
+      vm.movieDetailedIndex = movieid;
+      vm.movieDetails = vm.activePage[vm.movieDetailedIndex];
     }
 
     function nextMovieDetails() {
-      vm.movieDetailedIndex += 1;
-      vm.movieDetails = vm.movies[vm.movieDetailedIndex];
+      if(vm.movieDetailedIndex < 19) {
+        vm.movieDetailedIndex += 1;
+        vm.movieDetails = vm.activePage[vm.movieDetailedIndex];
+      } else {
+        vm.pageIndex +=1;
+        vm.getMovies(vm.pageIndex);
+        vm.movieDetailedIndex = 0;
+        vm.movieDetails = vm.activePage[vm.movieDetailedIndex];
+      }
     }
 
   }
